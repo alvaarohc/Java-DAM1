4973d3c1db22fbaacf416f74a955f4e4fae173f3
Alvaro Hernandez <alvarohcolino@gmail.com>
2024-10-17 11:34:19 +0200

    add Iteraciones.java

--- src/intro/OperadorTernario.java
+++ src/intro/OperadorTernario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package intro;
 
 import java.util.Scanner;
@@ -14,7 +10,7 @@ public class OperadorTernario {
 
     public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
-//
+// 
 //        System.out.print("Introduce a: ");
 //        int a = sc.nextInt();
 //        System.out.print("Introduce b: ");
